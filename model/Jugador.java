29d750a0146d94a11ff5c4f28266e4df98ff73bb
JH074 <00162622@uca.edu.sv>
2025-04-05 18:21:45 -0600

    Add: determinar quien tiene mas goles :sparkles: y arreglo de clases :pencil2:

--- model/Jugador.java
+++ model/Jugador.java
@@ -2,18 +2,24 @@ package model;
 
 public class Jugador {
 
+    private int id;
     private String nombre;
     private int golesMarcados;
     private int partidosJugados;
 
     // Constructor
-    public Jugador(String nombre, int golesMarcados, int partidosJugados) {
+    public Jugador(int id, String nombre, int golesMarcados, int partidosJugados) {
+        this.id = id;
         this.nombre = nombre;
         this.golesMarcados = golesMarcados;
         this.partidosJugados = partidosJugados;
     }
 
     // Getters
+    public int getId() {
+        return id;
+    }
+
     public String getNombre() {
         return nombre;
     }
@@ -26,10 +32,27 @@ public class Jugador {
         return partidosJugados;
     }
 
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setGolesMarcados(int golesMarcados) {
+        this.golesMarcados = golesMarcados;
+    }
+
+    public void setPartidosJugados(int partidosJugados) {
+        this.partidosJugados = partidosJugados;
+    }
+
     // toString override
     @Override
     public String toString() {
-        return "Jugador{" +
+        return "Jugador{ " +
                 "Nombre: " + nombre +
                 ", Goles marcados: " + golesMarcados +
                 ", Partidos jugados: " + partidosJugados +
